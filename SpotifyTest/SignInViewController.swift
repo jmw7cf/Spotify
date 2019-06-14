04fcc98ccb21871fceb86a6450983358f3198cfb
Julia Wopata <julia.wopata@gmail.com>
2019-06-14 09:52:30 -0500

    First commit

---
+++ SpotifyTest/SignInViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SignInViewController.swift
+//  SpotifyTest
+//
+//  Created by Julia Wopata on 6/9/19.
+//  Copyright © 2019 Julia Wopata. All rights reserved.
+//
+
+import Foundation
---
+++ SpotifyTest/ViewController.swift
@@ -0,0 +1,240 @@
+//
+//  ViewController.swift
+//  SpotifyTest
+//
+//  Created by Julia Wopata on 6/9/19.
+//  Copyright © 2019 Julia Wopata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, SPTSessionManagerDelegate, SPTAppRemoteDelegate, SPTAppRemotePlayerStateDelegate {
+    fileprivate let SpotifyClientID = "6e0041c303ba4062bd3bd2b0d19322ab"
+    fileprivate let SpotifyRedirectURI = URL(string: "julias-music-app://spotify-login-callback")!
+    
+    lazy var configuration: SPTConfiguration = {
+        let configuration = SPTConfiguration(clientID: SpotifyClientID, redirectURL: SpotifyRedirectURI)
+        configuration.playURI = ""
+        configuration.tokenSwapURL = URL(string: "https://juliaspotifytest.herokuapp.com/api/token")
+        configuration.tokenRefreshURL = URL(string: "https://juliaspotifytest.herokuapp.com/api/refresh_token")
+        return configuration
+    }()
+    
+    lazy var sessionManager: SPTSessionManager = {
+        let manager = SPTSessionManager(configuration: configuration, delegate: self)
+        return manager
+    }()
+    
+    lazy var appRemote: SPTAppRemote = {
+        let appRemote = SPTAppRemote(configuration: configuration, logLevel: .debug)
+        appRemote.delegate = self
+        return appRemote
+    }()
+    
+    fileprivate var lastPlayerState: SPTAppRemotePlayerState?
+    
+    fileprivate lazy var connectLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Connect your Spotify account"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    fileprivate lazy var connectButton = ConnectButton(title: "CONNECT")
+    fileprivate lazy var disconnectButton = ConnectButton(title: "DISCONNECT")
+    
+    fileprivate lazy var pauseAndPlayButton: UIButton = {
+        let button = UIButton()
+        button.addTarget(self, action: #selector(didTapPauseOrPlay), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    fileprivate lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    fileprivate lazy var trackLabel: UILabel = {
+        let trackLabel = UILabel()
+        trackLabel.translatesAutoresizingMaskIntoConstraints = false
+        trackLabel.textAlignment = .center
+        return trackLabel
+    }()
+    
+    func update(playerState: SPTAppRemotePlayerState) {
+        if lastPlayerState?.track.uri != playerState.track.uri {
+            fetchArtwork(for: playerState.track)
+        }
+        lastPlayerState = playerState
+        trackLabel.text = playerState.track.name
+        if playerState.isPaused {
+            pauseAndPlayButton.setImage(UIImage(named: "play"), for: .normal)
+        } else {
+            pauseAndPlayButton.setImage(UIImage(named: "pause"), for: .normal)
+        }
+    }
+    
+    func playerStateDidChange(_ playerState: SPTAppRemotePlayerState) {
+        update(playerState: playerState)
+    }
+    
+    // SPTAppRemoteDelegate
+    func appRemoteDidEstablishConnection(_ appRemote: SPTAppRemote) {
+        updateViewBasedOnConnected()
+        appRemote.playerAPI?.delegate = self
+        appRemote.playerAPI?.subscribe(toPlayerState: { (success, error) in
+            if let error = error {
+                print("Error subscribing to player state:" + error.localizedDescription)
+            }
+        })
+        fetchPlayerState()
+    }
+    
+    func appRemote(_ appRemote: SPTAppRemote, didDisconnectWithError error: Error?) {
+        updateViewBasedOnConnected()
+        lastPlayerState = nil
+    }
+    
+    func appRemote(_ appRemote: SPTAppRemote, didFailConnectionAttemptWithError error: Error?) {
+        updateViewBasedOnConnected()
+        lastPlayerState = nil
+    }
+    
+    // SPTSessionManagerDelegate
+    func sessionManager(manager: SPTSessionManager, didFailWith error: Error) {
+        presentAlertController(title: "Authorization Failed", message: error.localizedDescription, buttonTitle: "Bummer")
+    }
+    
+    func sessionManager(manager: SPTSessionManager, didInitiate session: SPTSession) {
+        presentAlertController(title: "Session Renewed", message: session.description, buttonTitle: "Sweet")
+    }
+    
+    // Actions
+    
+    @objc func didTapPauseOrPlay(_ button: UIButton) {
+        if let lastPlayerState = lastPlayerState, lastPlayerState.isPaused {
+            appRemote.playerAPI?.resume(nil)
+        } else {
+            appRemote.playerAPI?.pause(nil)
+        }
+    }
+    
+    @objc func didTapDisconnect(_ button: UIButton) {
+        if (appRemote.isConnected) {
+            appRemote.disconnect()
+        }
+    }
+    
+    @objc func didTapConnect(_ button: UIButton) {
+        /*
+         Scopes let you specify exactly what types of data your application wants to
+         access, and the set of scopes you pass in your call determines what access
+         permissions the user is asked to grant.
+         For more information, see https://developer.spotify.com/web-api/using-scopes/.
+         */
+        let scope: SPTScope = [.appRemoteControl, .playlistReadPrivate]
+        
+        if #available(iOS 11, *) {
+            // Use this on iOS 11 and above to take advantage of SFAuthenticationSession
+            sessionManager.initiateSession(with: scope, options: .clientOnly)
+        } else {
+            // Use this on iOS versions < 11 to use SFSafariViewController
+            sessionManager.initiateSession(with: scope, options: .clientOnly, presenting: self)
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.white
+        
+        view.addSubview(connectLabel)
+        view.addSubview(connectButton)
+        view.addSubview(disconnectButton)
+        view.addSubview(imageView)
+        view.addSubview(trackLabel)
+        view.addSubview(pauseAndPlayButton)
+        
+        let constant: CGFloat = 16.0
+        
+        connectButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        connectButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        disconnectButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        disconnectButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50).isActive = true
+        
+        connectLabel.centerXAnchor.constraint(equalTo: connectButton.centerXAnchor).isActive = true
+        connectLabel.bottomAnchor.constraint(equalTo: connectButton.topAnchor, constant: -constant).isActive = true
+        
+        imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        imageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 64).isActive = true
+        imageView.bottomAnchor.constraint(equalTo: trackLabel.topAnchor, constant: -constant).isActive = true
+        
+        trackLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        trackLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: constant).isActive = true
+        trackLabel.bottomAnchor.constraint(equalTo: connectLabel.topAnchor, constant: -constant).isActive = true
+        
+        pauseAndPlayButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        pauseAndPlayButton.topAnchor.constraint(equalTo: trackLabel.bottomAnchor, constant: constant).isActive = true
+        pauseAndPlayButton.widthAnchor.constraint(equalToConstant: 50)
+        pauseAndPlayButton.heightAnchor.constraint(equalToConstant: 50)
+        pauseAndPlayButton.sizeToFit()
+        
+        connectButton.sizeToFit()
+        disconnectButton.sizeToFit()
+        
+        connectButton.addTarget(self, action: #selector(didTapConnect(_:)), for: .touchUpInside)
+        disconnectButton.addTarget(self, action: #selector(didTapDisconnect(_:)), for: .touchUpInside)
+        
+        updateViewBasedOnConnected()
+    }
+    
+    func updateViewBasedOnConnected() {
+        if (appRemote.isConnected) {
+            connectButton.isHidden = true
+            disconnectButton.isHidden = false
+            connectLabel.isHidden = true
+            imageView.isHidden = false
+            trackLabel.isHidden = false
+            pauseAndPlayButton.isHidden = false
+        } else {
+            disconnectButton.isHidden = true
+            connectButton.isHidden = false
+            connectLabel.isHidden = false
+            imageView.isHidden = true
+            trackLabel.isHidden = true
+            pauseAndPlayButton.isHidden = true
+        }
+    }
+    
+    func fetchArtwork(for track:SPTAppRemoteTrack) {
+        appRemote.imageAPI?.fetchImage(forItem: track, with: CGSize.zero, callback: { [weak self] (image, error) in
+            if let error = error {
+                print("Error fetching track image: " + error.localizedDescription)
+            } else if let image = image as? UIImage {
+                self?.imageView.image = image
+            }
+        })
+    }
+    
+    func fetchPlayerState() {
+        appRemote.playerAPI?.getPlayerState({ [weak self] (playerState, error) in
+            if let error = error {
+                print("Error getting player state:" + error.localizedDescription)
+            } else if let playerState = playerState as? SPTAppRemotePlayerState {
+                self?.update(playerState: playerState)
+            }
+        })
+    }
+    
+    fileprivate func presentAlertController(title: String, message: String, buttonTitle: String) {
+        let controller = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: buttonTitle, style: .default, handler: nil)
+        controller.addAction(action)
+        present(controller, animated: true)
+    }
+
+}
+
